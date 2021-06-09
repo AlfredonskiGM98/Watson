02dc9fb47831a47684ff177b4ae24fec472d6631
AlfredonskiGM98 <82979196+AlfredonskiGM98@users.noreply.github.com>
2021-06-09 13:23:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function newImage (){
 	var image = document.getElementById("myImage") ;
 	if(image.src.match("image2")){
-		image.src = "images/image1.jpg";
+		image.src = "image1.jpg";
 		}else{
-		image.src = "images/image2.jpg"
+		image.src = "image2.jpg"
 		}
 	}
